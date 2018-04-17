140b122f057474e22e8ab88f1d798ebe2a9ad90c
Evan Power <evan.power44@gmail.com>
2018-04-17 09:55:17 -0400

    Fixed some stuff

--- Bunny Project/Bunny.cpp
+++ Bunny Project/Bunny.cpp
@@ -1,29 +1,28 @@
 ﻿#include "stdafx.h"
 #include "Bunny.h"
 #include "rand.h"
-#include <fstream>
 #include <iostream>
 
 constexpr int MAX_NONRADIOACTIVE_AGE = 10;
 constexpr int MAX_RADIOACTIVE_AGE = 50;
 
-Bunny::Bunny() 
+
+std::vector<std::string> Bunny::names = { "Arnold", "Bam Bam", "Geoffrey", "Snickers", "Cosmo", 
+	"Spot", "Bilbo", "Darrow", "Rex", "Pluto", "Shakespeare", "Spice", "Coda", "Mars", "Thumper", 
+	"Bomber", "Dough Boy", "Tripper", "Chad", "Dalton", "Butter", "Gucci", "Gremlin", "Jersey",
+	"Stormie", "Smudge", "Gringo", "Anastasia", "Fluffy", "Bumper", "Pepsi", "Ajax", "Beefeater",
+	"Alf", "Beebo", "Cheezels", "Tequila", "Mercedes", "Nibbles", "Pancake", "Paris", "Stripe"};
+
+bool Bunny::IsDying()
 {
-	age = 0;
-	sex = rand_int(0, 1) ? MALE : FEMALE;
-	radioactive_mutant_vampire_bunny = rand_int(1,100) <= 2;
-	if (radioactive_mutant_vampire_bunny)
-	{
-		name = "Radioactive Mutant Vampire Bunny " + name;
-	}
-	
-	std::cout << name << "was born!";
-	color = static_cast<Color>(rand_int(static_cast<int>(WHITE), static_cast<int>(SPOTTED)));
-	
+	const bool nonradioactive_dying = age > MAX_NONRADIOACTIVE_AGE && !radioactive_mutant_vampire_bunny;
+	const bool radioactive_dying = age > MAX_RADIOACTIVE_AGE && radioactive_mutant_vampire_bunny;
+	return  nonradioactive_dying || radioactive_dying;
 }
 
-Bunny::Bunny(const Bunny &mother)
+Bunny::Bunny() 
 {
+	name = names[rand_int(0, (int)names.size() - 1)];
 	age = 0;
 	sex = rand_int(0, 1) ? MALE : FEMALE;
 	radioactive_mutant_vampire_bunny = rand_int(1, 100) <= 2;
@@ -31,14 +30,27 @@ Bunny::Bunny(const Bunny &mother)
 	{
 		name = "Radioactive Mutant Vampire Bunny " + name;
 	}
+	
+	std::cout << name << " was born!" << std::endl;
+	color = static_cast<Color>(rand_int(static_cast<int>(WHITE), static_cast<int>(SPOTTED)));	
+}
 
-	std::cout << name << "was born!";
+Bunny::Bunny(const Bunny &mother)
+{
+	name = names[rand_int(0, (int)names.size() - 1)];
+	age = 0;
+	sex = rand_int(0, 1) ? MALE : FEMALE;
+	radioactive_mutant_vampire_bunny = rand_int(1,100) <= 2;
+	if (radioactive_mutant_vampire_bunny)
+	{
+		name = "Radioactive Mutant Vampire Bunny " + name;
+	}
+
+	std::cout << name << " was born!" << std::endl;
 	color = mother.color;
 }
 
-bool Bunny::IsDying()
+Bunny::~Bunny()
 {
-	const bool nonradioactive_dying = age > MAX_NONRADIOACTIVE_AGE && !radioactive_mutant_vampire_bunny;
-	const bool radioactive_dying = age > MAX_RADIOACTIVE_AGE && radioactive_mutant_vampire_bunny;
-	return  nonradioactive_dying || radioactive_dying;
+	std::cout << GetName() << " has died!" << std::endl;
 }
--- Bunny Project/Bunny.h
+++ Bunny Project/Bunny.h
@@ -1,17 +1,28 @@
 ﻿#pragma once
 #include <string>
 #include "Types.h"
+#include "rand.h"
 
 class Bunny 
 {
-public:
+	static std::vector<std::string> names;
 	Sex sex;
 	Color color;
 	unsigned int age;
 	std::string name;
 	bool radioactive_mutant_vampire_bunny;
-	bool IsDying();
 public:
+	Sex GetSex() { return sex; }
+	Color GetColor() { return color; }
+	int GetAge() { return age; }
+	std::string GetName() { return name; }
+	bool GetRadioactivity() { return radioactive_mutant_vampire_bunny; }
+
+	void IncrementAge() { age++; }
+	void SetRadioactivity(bool new_radioactivity) { radioactive_mutant_vampire_bunny = new_radioactivity; }
+	bool IsDying();
+
 	Bunny();
 	Bunny(const Bunny &mother);
+	~Bunny();
 };
